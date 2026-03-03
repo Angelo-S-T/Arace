0e1d97141a9525916f98b47324d24bc4643ddfb9
20232tiimi0128 <salveaquiu@gmail.com>
2026-03-03 16:20:04 -0300

    Nav inferior

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,6 +27,10 @@ android {
             )
         }
     }
+
+    viewBinding{
+        enable = true
+    }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
--- app/src/main/java/com/example/homepage/MainActivity.kt
+++ app/src/main/java/com/example/homepage/MainActivity.kt
@@ -5,16 +5,45 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.example.homepage.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //Onde vai implementar as telas do menu
+        //Por exemplo:
+        //R.id.explorar -> replaceFragment(fragmento1()) significa que o botão com id "explorar"
+        //vai direcionar a um fragment "fragmento1"
+
+
+        /*!replaceFragment(Home())
+
+            binding.bottomNavigationView.setOnItemSelectedListener {
+                when(it.itemId){
+                    R.id.explorar -> replaceFragment(fragmento1())
+                    R.id.home -> replaceFragment(fragmento2())
+                    R.id.fav -> replaceFragment(fragmento2())
+
+                    else ->{
+
+                    }
+                }
+                true
+            }
+
+        }*/
+
+
+    private fun replaceFragment(fragment: Fragment){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout,fragment)
+        fragmentTransaction.commit()
     }
 }
